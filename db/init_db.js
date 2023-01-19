9eac39a151af46f5986d8b56c33854b10128cd3b
PeterN <peternguyen628@yahoo.com>
2023-01-19 14:31:21 -0600

    feat(models/user): adds create users and bcrypt

--- db/init_db.js
+++ db/init_db.js
@@ -1,17 +1,41 @@
+const { createUser } = require('./models/user');
 const {
   client,
   // declare your model imports here
   // for example, User
 } = require('./');
 
-async function buildTables() {
+async function dropTables() {
+  try {
+    console.log("Dropping all tables!!!!!")
+
+    await client.query(`
+      DROP TABLE IF EXISTS users;
+    `);
+
+    console.log("Finished dropping tables!!!!!")
+  } catch (error) {
+    console.error("ERROR DROPPING TABLES!!!!!")
+  }
+}
+
+async function createTables() {
   try {
     client.connect();
+    console.log("Starting to create tables!!!!!")
 
-    // drop tables in correct order
+    await client.query(`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      email VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL
+    );
+    `);
 
-    // build tables in correct order
+    console.log("Finished creating tables!!!!!")
+
   } catch (error) {
+    console.error("ERROR CREATING TABLES!!!!!")
     throw error;
   }
 }
@@ -21,12 +45,33 @@ async function populateInitialData() {
     // create useful starting data by leveraging your
     // Model.method() adapters to seed your db, for example:
     // const user1 = await User.createUser({ ...user info goes here... })
+    const usersToCreate = [
+      { email: "albert@gmail.com", password: "bertie99" },
+      { email: "sandra@gmail.com", password: "sandra123" },
+      { email: "glamgal@gmail.com", password: "glamgal123" },
+    ]
+    const users = await Promise.all(usersToCreate.map(createUser))
+
+    console.log("Users created:")
+    console.log(users)
+    console.log("Finished creating users!")
   } catch (error) {
+    console.error("Error creating users!")
     throw error;
   }
 }
 
-buildTables()
-  .then(populateInitialData)
+async function rebuildDB() {
+  try {
+    await dropTables()
+    await createTables()
+    await populateInitialData()
+  } catch (error) {
+    console.log("Error during rebuildDB")
+    throw error
+  }
+}
+
+rebuildDB()
   .catch(console.error)
   .finally(() => client.end());
--- db/models/user.js
+++ db/models/user.js
@@ -1,11 +1,68 @@
 // grab our db client connection to use with our adapters
 const client = require('../client');
+const bcrypt = require("bcrypt");
+
+async function getUsersByEmail(email) {
+  /* this adapter should fetch a list of users from your db */
+
+  try {
+    const { rows: [user] } = await client.query(`
+    SELECT *
+    FROM users
+    WHERE email = $1
+    `, [email])
+    return user;
+  } catch (error) {
+    console.error("ERROR GETTING USERS BY EMAIL!!!!!")
+  }
+}
+
+async function getUsers({ email, password }) {
+  /* this adapter should fetch a list of users from your db */
+  if (!email || !password) {
+    return;
+  }
+
+  try {
+    const user = await getUsersByEmail(email)
+    const hashedPassword = user.password;
+    let matchedPassword = await bcrypt.compare(password, hashedPassword);
+
+    if (matchedPassword) {
+      delete hashedPassword;
+      return user;
+    }
+    else {
+      return;
+    }
+
+  } catch (error) {
+    console.error("ERROR GETTING USER!!!!!")
+  }
+}
+
+async function createUser({ email, password }) {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  try {
+    const { rows: [user] } = await client.query(`
+      INSERT INTO users (email,password)
+      VALUES($1,$2)
+      ON CONFLICT (email) DO NOTHING
+      RETURNING *;
+    `, [email, hashedPassword]);
+    delete user.password;
+
+    return user;
+  } catch (error) {
+    console.error("ERROR CREATING USER!!!!!")
+  }
+}
 
 module.exports = {
   // add your database adapter fns here
-  getAllUsers,
+  getUsersByEmail,
+  getUsers,
+  createUser,
 };
-
-async function getAllUsers() {
-  /* this adapter should fetch a list of users from your db */
-}
