4e3fef2c05b9b6042246fe778715bfb1bda08215
Matthew Waskin <matthew@waskin.com>
2023-01-20 09:16:47 -0500

    feat (state management): added StateContext using useReducer and useContext to put state into context

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './components';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./components";
+import { StateProvider } from "./StateContext";
 // css stylesheets can be created for each component
 // place them in the src/style directory, and import them like this:
-import './style/index.css';
+import "./style/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<StateProvider>
+			<App />
+		</StateProvider>
+	</React.StrictMode>
 );
