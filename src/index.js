71085d205c492723b0a2989c5479333549ca0d7a
thekidsparrow <aveshart@gmail.com>
2023-01-24 09:08:52 -0800

    removing catco context bc confuse

--- src/index.js
+++ src/index.js
@@ -6,14 +6,11 @@ import { BrowserRouter } from "react-router-dom";
 // css stylesheets can be created for each component
 // place them in the src/style directory, and import them like this:
 import "./style/index.css";
-import { CatcoProvider } from "./CatcoContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<CatcoProvider>
-				<App />
-			</CatcoProvider>
+			<App />
 		</BrowserRouter>
 	</React.StrictMode>
 );
