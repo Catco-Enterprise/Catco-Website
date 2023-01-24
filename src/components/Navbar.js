6d3ff9d3717b3f538a916a99cb916ff3620a0739
thekidsparrow <aveshart@gmail.com>
2023-01-24 09:39:46 -0800

    removing goopy statecontext bc not g00d

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,19 @@
 import React from "react";
-import { Link } from 'react-router-dom'
-import { useForState, useStateDispatch } from "../StateContext";
+import { Link, useNavigate } from 'react-router-dom'
 import '../style/Navbar.css'
 
-
-
 const Navbar = () => {
+    const navigate = useNavigate();
 
     const linkStyle = {
-        textDecoration : "none",
+        textDecoration: "none",
         margin: "7px"
     }
 
-    const dispatch = useStateDispatch();
-    const state = useForState();
-
+    function handleSignOut() {
+        localStorage.removeItem('token');
+        navigate('/');
+    }
 
     return (
         <div className="navbar-container">
@@ -24,17 +23,12 @@ const Navbar = () => {
                 <Link style={linkStyle} to="/Products">Products</Link>
                 <Link style={linkStyle} to="/Login">Login</Link>
                 {state.token ? (
-                <div>
-                    <Link style={linkStyle} to='/' onClick={() => {
-                    localStorage.clear();
-                    dispatch({type: 'setToken', payload: ''});
-                    dispatch({type: 'setUser', payload: {}});
-                    console.log(state);
-                    }}>Sign Out</Link>
-                </div>
-                ) : null }
+                    <div>
+                        <Link style={linkStyle} to='/' onClick={handleSignOut}>Sign Out</Link>
+                    </div>
+                ) : null}
                 <Link style={linkStyle} to="/Cart">Cart</Link>
-                
+
             </nav>
         </div>
 
