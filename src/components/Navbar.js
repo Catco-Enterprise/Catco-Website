4fc43ed4002825b716a19419dbf94cef31575a66
thekidsparrow <aveshart@gmail.com>
2023-01-24 09:43:39 -0800

    removing statecontext cause we prop people now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from 'react-router-dom'
 import '../style/Navbar.css'
 
-const Navbar = () => {
-    const navigate = useNavigate();
+function Navbar() {
+    // Do the token be around here?
+    const token = localStorage.getItem('token');
 
     const linkStyle = {
         textDecoration: "none",
         margin: "7px"
     }
 
+    // Lord bless my soul, I banish thy token from thy browser
+    // and establish this user as officially signed out
+    // HALLELUJAH!
     function handleSignOut() {
         localStorage.removeItem('token');
-        navigate('/');
     }
 
     return (
@@ -22,7 +25,8 @@ const Navbar = () => {
                 <Link style={linkStyle} to="/">Home</Link>
                 <Link style={linkStyle} to="/Products">Products</Link>
                 <Link style={linkStyle} to="/Login">Login</Link>
-                {state.token ? (
+                {/* If the token be around here, then we is gonna help you sign out */}
+                {token ? (
                     <div>
                         <Link style={linkStyle} to='/' onClick={handleSignOut}>Sign Out</Link>
                     </div>
