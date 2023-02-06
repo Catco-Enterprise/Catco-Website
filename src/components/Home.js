0d2d3e02cc846c40eb3b01a7b8e6325a36449512
Matthew Waskin <matthew@waskin.com>
2023-02-06 15:07:58 -0500

    added demo login button to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,51 @@
 import React from "react";
-import '../style/Home.css'
+import "../style/Home.css";
+import { login } from "../axios-services";
+import { useNavigate } from "react-router-dom";
 
+const Home = ({ setToken, setUser, setIsLoggedIn, setCartItems }) => {
+	const navigate = useNavigate();
+	async function handleClick(event) {
+		event.preventDefault();
 
+		const result = await login("albert@gmail.com", "bertie99");
 
-const Home = (token) => {
-    return (
-        <>
-            <h1>
-                Welcome to Catco
-            </h1>
-            <p>Welcome to Catco the ultimate e-commerce site for all things cats. Our mission is to provide the best experience for our furry cat parents. Click away on our products page and you will find everything you need.</p>
+		if (result.token) {
+			const userCart = result.user.activeOrder.products;
+			if (userCart.length) {
+				localStorage.setItem("cartItems", JSON.stringify(userCart));
+			}
+			localStorage.setItem("token", result.token);
 
+			setIsLoggedIn(true);
+			setToken(result.token);
+			setUser(result.user);
+			setCartItems(userCart);
 
+			navigate("/");
+		}
+	}
 
-        </>
-    )
-}
-
-
-
-
-
-
+	return (
+		<>
+			<h1>Welcome to Catco</h1>
+			<p>
+				Welcome to Catco the ultimate e-commerce site for all things cats. Our
+				mission is to provide the best experience for our furry cat parents.
+				Click away on our products page and you will find everything you need.
+			</p>
+			<hr />
+			<p>
+				Just here to demo the site? Use the button below to sign in with a demo
+				account, which will allow you to explore the site from the perspective
+				of a registered user without the hassle of creating your own account. We
+				hope you enjoy!
+			</p>
+			<p>
+				<button onClick={handleClick}>Demo Sign-In</button>
+			</p>
+		</>
+	);
+};
 
 export default Home;
