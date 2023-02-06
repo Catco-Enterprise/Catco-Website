060632656a77c431a142a0dc2fab7c80a8420f5c
Matthew Waskin <matthew@waskin.com>
2023-02-06 14:45:56 -0500

    fixed bug when rendering cart after deleting a product from it

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -9,15 +9,19 @@ import {
 } from "../axios-services";
 
 const SingleProduct = ({ product, activeOrder, cartItems, setCartItems }) => {
-	let prodQuantity = 0;
+	// let prodQuantity = 0;
 	const cartProdIdx = cartItems.findIndex((prod) => prod.id === product.id);
 
 	if (cartProdIdx > -1) {
-		prodQuantity = cartItems[cartProdIdx].quantity;
+		product.quantity = cartItems[cartProdIdx].quantity;
+		// prodQuantity = cartItems[cartProdIdx].quantity;
+	} else {
+		product.quantity = 0;
 	}
-	const singleProd = { ...product, quantity: prodQuantity };
+	// const singleProd = { ...product, quantity: prodQuantity };
 
-	const [singProd, setSingProd] = useState(singleProd);
+	const [singProd, setSingProd] = useState(product);
+	// const [singProd, setSingProd] = useState(singleProd);
 
 	function handleMinusQuantity() {
 		const newQty = singProd.quantity - 1;
